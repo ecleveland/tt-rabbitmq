03f8a7ebb6b73d88ccbfe77ff9d33db43d3d09ab
Eric Cleveland <ecleveland@trintech.com>
2018-02-12 14:36:30 -0600

    second commit

---
+++ Receive/Receive.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace Receive
+{
+    class Receive
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "rabbitmq.trin.tech", UserName="user", Password="1Trintech2018" };
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(
+                        queue: "hello",
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null
+                    );
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (IModel, ea) =>
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine(" [x] Received {0}", message);
+                    };
+                    channel.BasicConsume(
+                        queue: "hello",
+                        autoAck: true,
+                        consumer: consumer);
+                    
+                    Console.WriteLine(" Press [enter] to exit.");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
---
+++ Send/Send.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace Send
+{
+    class Send
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine(" Type message to send.");
+                SendMessage(Console.ReadLine());
+            }
+        }
+
+        private static void SendMessage(string messageText)
+        {
+            var factory = new ConnectionFactory() { HostName = "rabbitmq.trin.tech", UserName = "user", Password = "1Trintech2018" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "hello",
+                                         durable: false,
+                                         exclusive: false,
+                                         autoDelete: false,
+                                         arguments: null);
+
+                    string message = string.Format("Sent Message: '{0}'", messageText);
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "",
+                                         routingKey: "hello",
+                                         basicProperties: null,
+                                         body: body);
+
+                    Console.WriteLine(" [x] Sent {0}", message);
+                }
+            }
+        }
+    }
+}
